47d131e2f4be67913e8dcccb5ce05160743bc1ce
haneenfadi <eduai3241@gmail.com>
2026-06-01 21:24:20 +0300

    add settings and init files

---
+++ src/config/settings.py
@@ -0,0 +1,9 @@
+class settings:
+
+    url = {
+        "lob": "https://www.lob.gov.jo/?v=1&lang=ar#!/DraftDetails?DraftID=10760",
+    }
+
+    pdf = {
+        "pdf_1": r"src\data\raw\قانون_العمل_رقم_8_لسنة_1996_وتعديلاته.pdf",
+    }
